a313a159102d1bc896f8a6320d6caee4a3293af8
Parth Salvi <parth.salvi@entercomsadmin>
2020-05-17 01:01:57 +0530

    modified servies as per UI

--- covid19_dashboard-master/py_server.py
+++ covid19_dashboard-master/py_server.py
@@ -1,5 +1,7 @@
 from flask import Flask ,request, jsonify, make_response
 from sqlalchemy import create_engine, update, select, insert
+import pandas as pd
+import numpy as np
 app = Flask(__name__)
 import json
 from cov19Engine import Cov19
@@ -21,14 +23,22 @@ def _corsify_actual_response(response):
 
 
 def top10ConfirmrdCountries():
-    data=c19.highest_10_cntrs_confirmed.to_dict('r')
-    return _corsify_actual_response(jsonify(data))
+    if request.method == "OPTIONS":
+        return _build_cors_prelight_response()
+    elif request.method in ["GET","POST"]:
+        data=c19.highest_10_cntrs_confirmed.to_dict('r')
+        return _corsify_actual_response(jsonify(data))
+    else:
+        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
 
 def top10DeceasedCountries():
     data=c19.highest_10_cntrs_deceased.to_dict('r')
     return _corsify_actual_response(jsonify(data))
 
 def totalValues():
+    if request.method == "OPTIONS":
+        print('inside option')
+        return _build_cors_prelight_response()
     data = {'total confirmed': int(c19.df_confirmed_total[-1]),
             'total deceased': int(c19.df_deaths_total[-1]),
             'total recovered': int(c19.df_recovered_total[-1])
@@ -71,16 +81,34 @@ def lineChartDaily():
         return _corsify_actual_response(jsonify(data))
 
 def lineChartCumulative():
+    if request.method == "OPTIONS":
+        print('inside option')
+        return _build_cors_prelight_response()
     conf = c19.df_confirmed_total
     death = c19.df_deaths_total
     reco = c19.df_recovered_total
-
     if (list(conf.index.values) == list(death.index.values) == list(reco.index.values)):
+        conf.index = pd.to_datetime(conf.index)
+        conf = conf.reset_index()
+        conf['index'] = pd.DatetimeIndex(conf['index']).astype(np.int64) // 10**9
+        conf = conf.values.tolist()
+        ###
+        death.index = pd.to_datetime(death.index)
+        death = death.reset_index()
+        death['index'] = pd.DatetimeIndex(death['index']).astype(np.int64) // 10**9
+        death = death.values.tolist()
+        ###
+
+        reco.index = pd.to_datetime(reco.index)
+        reco  =reco.reset_index()
+        reco['index'] = pd.DatetimeIndex(reco['index']).astype(np.int64) // 10**9
+        reco = reco.values.tolist()
         print("plot indexes are equal")
-        data={'dates': (list(conf.index.values)),
-              'conf': conf.tolist(),
-              'death': death.tolist(),
-              'recovered': reco.tolist()}
+        data={#'dates': (list(conf.index.values)),
+              'conf': conf,#.tolist(),
+              'death': death,#.tolist(),
+              'recovered': reco,#.tolist()}
+        }
         return _corsify_actual_response(jsonify(data))
     else:
         print(len(list(conf.index.values)))
